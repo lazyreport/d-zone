0f1eb53d03aff541ad0a3df8d8e7dca9f29c3d43
shenrenrong <charixiama@gmail.com>
2022-03-23 18:38:57 +0900

    rr

--- src/components/PointOverview/PointOverview.js
+++ src/components/PointOverview/PointOverview.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Form from "react-bootstrap/Form";
